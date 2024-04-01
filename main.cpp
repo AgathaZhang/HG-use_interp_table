3a80fde6b6191de37e51a7f3213436f81105f09a
Fred <fred0412@live.com>
2024-04-01 11:12:07 +0800

    完成interp代码，TODO 1.边界点插值失真的问题 2.match erro总是出现的问题，增加特殊情况的匹配机制

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ typedef struct DataPoint
     double weight_y = 0.0;
 } DataPoint;
 
-extern std::vector<DataPoint> dataPoints;
+
 
 std::vector<DataPoint> dataPoints;
 
@@ -245,7 +245,7 @@ DataPoint find_grid_ID(double x, double y, const std::vector<DataPoint>& dataPoi
 }
 
 std::vector<double> interp_1(DataPoint p1, DataPoint p2, double x, double y)
-{   std::vector<double> weigh_p1(2), weigh_p2(2), weigh_ans(2);
+{   std::vector<double> weigh_p1(2), weigh_p2(2), weigh_ans(4);         // weigh 向量的前两位分别表示weigh_x 和 weigh_y ，后两位表示x, y点在x = x的直线正交 到 与p1 p2 连线的交点坐标
     weigh_p1[0] = p1.weight_x;
     weigh_p1[1] = p1.weight_y;
     weigh_p2[0] = p2.weight_x;
@@ -253,18 +253,42 @@ std::vector<double> interp_1(DataPoint p1, DataPoint p2, double x, double y)
     // weigh_ans[0] = abs((x - p1.mean_x))/abs((p1.mean_x - p2.mean_x));
     weigh_ans[0] = abs((x - p1.mean_x))/abs((p1.mean_x - p2.mean_x))*weigh_p2[0] + abs((x - p2.mean_x))/abs((p1.mean_x - p2.mean_x))*weigh_p1[0];
     weigh_ans[1] = abs((x - p1.mean_x))/abs((p1.mean_x - p2.mean_x))*weigh_p2[1] + abs((x - p2.mean_x))/abs((p1.mean_x - p2.mean_x))*weigh_p1[1];
+    // TODO 第二次插值的时候肯定要用到 修正的y坐标 用于权值估计 其实由于两条已x方向的直线在正交系下的斜率不一样 因此y方向的直线两端的伸缩补偿不一样
+    //      输出一个位置距离 y = kx + b 两点式，计算欧氏距离，这条直线肯定是过xy点的//验证一下是否过该直线
+    //      (y-y2)/(y1-y2) = (x-x2)/(x1-x2) 两点式
+    double y_new;
+    y_new = (x - p2.mean_x)/(p1.mean_x - p2.mean_x)*(p1.mean_y - p2.mean_y) + p2.mean_y;
+    weigh_ans[2] = x;
+    weigh_ans[3] = y_new;
 
     return weigh_ans;
 }
 
 std::vector<double> interp_2(std::vector<double> p1, std::vector<double> p2, double x, double y)
 {
-    std::vector<double> weigh_ans(2)
-    
+    std::vector<double> weigh_ans(2);
+    auto& p1_weigh_x = p1[0];
+    auto& p1_weigh_y = p1[1];
+    auto& p1_set_x = p1[2];
+    auto& p1_set_y = p1[3];
+
+    auto& p2_weigh_x = p2[0];
+    auto& p2_weigh_y = p2[1];
+    auto& p2_set_x = p2[2];
+    auto& p2_set_y = p2[3];
+
+    // 求3个欧氏距离用于计算权重
+    double total, left, right;
+    total = sqrt(pow(p2_set_x - p1_set_x, 2) + pow(p2_set_y - p1_set_y, 2));
+    left = sqrt(pow(x - p2_set_x, 2) + pow(y - p2_set_y, 2));
+    right = sqrt(pow(x - p1_set_x, 2) + pow(y - p1_set_y, 2));
+    weigh_ans[0] = left/total*p1_weigh_x + right/total*p2_weigh_x;
+    weigh_ans[1] = left/total*p1_weigh_y + right/total*p2_weigh_y;
+    return weigh_ans;
 }
 
 
-int Interp_coords(const std::vector<DataPoint> &dataPoints, double x, double y)
+std::vector<double> Interp_coords(const std::vector<DataPoint> &dataPoints, double x, double y)
 {
 
     DataPoint this_grid = find_grid_ID(x, y, dataPoints);     // 找到最近的格点 // 返回基于格点的方位向量 // 根据方位向量确定包围
@@ -287,27 +311,31 @@ int Interp_coords(const std::vector<DataPoint> &dataPoints, double x, double y)
         left_up_point = four_point[i];}
     
     // 开始插值
-    std::vector<double> r1(2), r2(2), P(2);
-    r1 = interp(left_up_point, right_up_point, x, y);
-    r2 = interp(left_down_point, right_down_point, x, y);
+    std::vector<double> r1(4), r2(4), P(2);
+    r1 = interp_1(left_up_point, right_up_point, x, y);        // 第一次插值输出的结果 向量的前两位表示weigh_x 和 weigh_y ，后两位表示x, y点坐标
+    r2 = interp_1(left_down_point, right_down_point, x, y);
     // P = interp(left_up_point, right_up_point, x, y);        // 这里要区分插值方向，第三次是插y值
-
-
-
-    
-    
-
-    printf("!w!!!!!!!!!!!!!!!!!!!zhang");
-    return 0;
+    P = interp_2(r1, r2, x, y);
+    printf("finished interp");
+    return  P;
 }
 
 int main()
 {   
 
-    double x = 1.13, y = 0.91;
+    // double x = 1.13, y = 0.91;
+    // double x = 0.56, y = 1.65;
+    // double x = 0.96, y = 1.19;
+    // double x = 0.15, y = 2.03;
+    double x = 1.30, y = 2.19;
+    
+
     extern std::vector<DataPoint> dataPoints;
     read_table();
-    Interp_coords(dataPoints, x, y);
-    printf("end coding zhang");
+    std::vector<double> weigh_out = Interp_coords(dataPoints, x, y);
+    double x_out = x*weigh_out[0];
+    double y_out = y*weigh_out[1];
+    
+    printf("result x:%f y:%f", x_out, y_out);
     return 0;
 }
